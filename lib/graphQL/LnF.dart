d6c978768a503a8f8f22282e41a5572168107d8b
Anshul072 <anshulmehta394@gmail.com>
2022-02-04 11:43:32 +0530

    lost and found queries and mutations

---
+++ lib/graphQL/LnF.dart
@@ -0,0 +1,42 @@
+class LnFQuery{
+  String createItem="""
+  mutation(\$itemInput: ItemInput!, \$images: [Upload!]){
+    createItem(ItemInput: \$itemInput, Images: \$images)
+  }
+  """;
+  String getItems="""
+   query(\$itemsFilter: [Category!]!,\$skip: Float!, \$take: Float!){
+    getItems(ItemsFilter: \$itemsFilter,skip: \$skip, take: \$take){
+    itemsList{
+      id
+      category
+      name
+      location
+      time
+      images
+      contact
+      user{
+        id
+        roll
+        name
+        mobile
+      }
+    }
+    total
+  }
+    getMe {
+    id
+  }
+  }
+  """;
+  String editItem="""
+  mutation(\$editItemInput: EditItemInput!, \$itemId: String!, \$editItemsImages: [Upload!]){
+  editItems(EditItemInput: \$editItemInput, ItemId: \$itemId, Images: \$editItemsImages)
+}
+  """;
+  String resolveItem="""
+  mutation(\$resolveItemItemId: String!){
+  resolveItem(ItemId: \$resolveItemItemId)
+}
+  """;
+}
