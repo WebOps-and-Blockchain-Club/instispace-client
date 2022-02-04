4cbf893a757d450987875768f9fa0dde0e0d51d6
Anshul072 <anshulmehta394@gmail.com>
2022-02-04 11:43:10 +0530

    netops queries and mutations

--- lib/graphQL/netops.dart
+++ lib/graphQL/netops.dart
@@ -7,7 +7,13 @@ class netopsQuery{
       comments {
         content
         id
+        createdBy {
+        name
+       }
       },
+      createdBy {
+        id
+      }
       title
       isHidden
       endTime
@@ -17,14 +23,78 @@ class netopsQuery{
         id
       }
       likeCount
+      photo
+      attachments
     }
     total
-  }
+  },
 }""";
   String createComment = """mutation(\$content: String!, \$netopId: String!){
   createComment(content: \$content, NetopId: \$netopId)
 }""";
-  String createNetop ="""mutation(\$newEventData: createNetopsInput!){
-  createNetop(NewEventData: \$newEventData)
-}""";
+  String createNetop ="""
+  mutation(\$newEventData: createNetopsInput!, \$image: Upload, \$attachments: [Upload!]){
+  createNetop(NewEventData: \$newEventData, Image: \$image, Attachments: \$attachments)
+}
+""";
+  String toggleLike ="""
+  mutation(\$toggleLikeNetopNetopId2: String!){
+  toggleLikeNetop(NetopId: \$toggleLikeNetopNetopId)
+}
+}
+  """;
+  String isLiked="""
+  query(\$isLikedNetopId2: String!){
+  isLiked(NetopId: \$isLikedNetopId2)
+}
+  """;
+  String getNetop="""
+  query(\$getNetopNetopId: String!){
+  getNetop(NetopId: \$getNetopNetopId){
+    likeCount
+    isStared
+    isLiked
+    comments {
+      content
+      id
+      createdBy {
+        name
+      }
+    }
+    createdBy {
+      id
+    }
+  },
+  getMe {
+    id
+  }
+}
+  """;
+  String toggleStar="""
+  mutation(\$toggleStarNetopId: String!){
+  toggleStar(NetopId: \$toggleStarNetopId)
+}
+  """;
+  String getMe="""
+  query{
+  getMe {
+    id
+  }
+}
+  """;
+  String deleteNetop="""
+  mutation(\$deleteNetopNetopId: String!){
+  deleteNetop(NetopId: \$deleteNetopNetopId)
+}
+  """;
+  String editNetop="""
+  mutation(\$editNetopNetopId: String!, \$editNetopsData: editNetopsInput!, \$editNetopAttachments: [Upload!], \$editNetopImage: Upload, \$tags: [String!]){
+  editNetop(NetopId: \$editNetopNetopId, EditNetopsData: \$editNetopsData, Attachments: \$editNetopAttachments, Image: \$editNetopImage, Tags: \$tags)
+}
+  """;
+  String reportNetop="""
+  mutation(\$description: String!, \$reportNetopNetopId: String!){
+  reportNetop(description: \$description, NetopId: \$reportNetopNetopId)
+}
+  """;
 }
