58bd0a0a0b5ad035093fd4aa1763605187ec6d09
Anshul072 <anshulmehta394@gmail.com>
2022-02-15 21:38:41 +0530

    announcement

--- lib/screens/home/Announcements/AnnouncementCard.dart
+++ lib/screens/home/Announcements/AnnouncementCard.dart
@@ -3,6 +3,7 @@ import 'package:client/graphQL/announcement_queries.dart';
 import 'package:client/graphQL/home.dart';
 import 'package:client/screens/home/Announcements/Announcement.dart';
 import 'package:client/screens/home/Announcements/home.dart';
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:client/screens/home/Announcements/SingleAnnouncement.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -56,154 +57,170 @@ class _AnnouncementCardState extends State<AnnouncementCard> {
             images = widget.announcement.images!.split(" AND ");
           }
 
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-            child: Card(
-              color: Color(0xFFDEDDFF),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SingleAnnouncement(
-                                announcement: Announcement(
-                                  title: widget.announcement.title,
-                                  description: widget.announcement.description,
-                                  endTime: widget.announcement.endTime,
-                                  id: widget.announcement.id,
-                                  images: widget.announcement.images,
-                                  createdByUserId:
-                                      widget.announcement.createdByUserId,
-                                  hostelIds: widget.announcement.hostelIds,
-                                ),
-                              )));
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(12.0, 10.0, 0.0, 0.0),
-                      child: Text(
-                        widget.announcement.title,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w500),
-                      ),
+          return ExpandableNotifier(
+            child: ScrollOnExpand(
+              child: ExpandablePanel(
+                theme: ExpandableThemeData(
+                  tapBodyToCollapse: true,
+                  tapBodyToExpand: true,
+                ),
+                expanded: SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 1,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width * 1,
+                  child: SingleAnnouncement(
+                    announcement: Announcement(
+                      title: widget.announcement.title,
+                      description: widget.announcement.description,
+                      endTime: widget.announcement.endTime,
+                      id: widget.announcement.id,
+                      images: widget.announcement.images,
+                      createdByUserId:
+                      widget.announcement.createdByUserId,
+                      hostelIds: widget.announcement.hostelIds,
                     ),
-                    if (images[0] != "")
-                      ClipRect(
-                        child: SizedBox(
-                          width: 400.0,
-                          child: CarouselSlider(
-                            items: images
-                                .map((item) => Container(
-                                      child: Center(
-                                        child: Image.network(
-                                          item,
-                                          fit: BoxFit.cover,
-                                          width: 400,
-                                        ),
-                                      ),
-                                    ))
-                                .toList(),
-                            options: CarouselOptions(
-                              enableInfiniteScroll: false,
-                            ),
+                  ),
+                ),
+                collapsed: Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                  child: Card(
+                    color: Color(0xFFDEDDFF),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12.0, 10.0, 0.0, 0.0),
+                          child: Text(
+                            widget.announcement.title,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500),
                           ),
                         ),
-                      ),
-                    if (images[0] == "")
-                      DescriptionTextWidget(
-                          text: widget.announcement.description),
-                    if (role == "ADMIN" ||
-                        role == "HAS" ||
-                        Id == widget.announcement.createdByUserId)
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 1),
-                        child: Row(
-                          children: [
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Color(0xFF6464DA),
-                                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                                  minimumSize: Size(40,24),
+                        if (images[0] != "")
+                          ClipRect(
+                            child: SizedBox(
+                              width: 400.0,
+                              child: CarouselSlider(
+                                items: images
+                                    .map((item) => Container(
+                                  child: Center(
+                                    child: Image.network(
+                                      item,
+                                      fit: BoxFit.cover,
+                                      width: 400,
+                                    ),
                                   ),
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        EditAnnouncements(
-                                          announcement: Announcement(
-                                            title: widget.announcement.title,
-                                            description:
-                                                widget.announcement.description,
-                                            endTime: widget.announcement.endTime,
-                                            id: widget.announcement.id,
-                                            images: widget.announcement.images,
-                                            createdByUserId: widget
-                                                .announcement.createdByUserId,
-                                            hostelIds:
-                                                widget.announcement.hostelIds,
-                                          ),
-                                          refetchAnnouncement: widget.refetchAnnouncement,
-                                        )));
-                              },
-                              child: Text(
-                                "Edit",
-                                style:
-                                    TextStyle(color: Colors.white, fontSize: 12.0),
+                                ))
+                                    .toList(),
+                                options: CarouselOptions(
+                                  enableInfiniteScroll: false,
+                                ),
                               ),
                             ),
-                            Mutation(
-                                options: MutationOptions(
-                                  document: gql(deleteAnnouncement),
+                          ),
+                        if (images[0] == "")
+                          DescriptionTextWidget(
+                              text: widget.announcement.description),
+                        if (role == "ADMIN" ||
+                            role == "HAS" ||
+                            Id == widget.announcement.createdByUserId)
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 5, 10, 1),
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color(0xFF6464DA),
+                                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                    minimumSize: Size(40,24),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            EditAnnouncements(
+                                              announcement: Announcement(
+                                                title: widget.announcement.title,
+                                                description:
+                                                widget.announcement.description,
+                                                endTime: widget.announcement.endTime,
+                                                id: widget.announcement.id,
+                                                images: widget.announcement.images,
+                                                createdByUserId: widget
+                                                    .announcement.createdByUserId,
+                                                hostelIds:
+                                                widget.announcement.hostelIds,
+                                              ),
+                                              refetchAnnouncement: widget.refetchAnnouncement,
+                                            )));
+                                  },
+                                  child: Text(
+                                    "Edit",
+                                    style:
+                                    TextStyle(color: Colors.white, fontSize: 12.0),
+                                  ),
                                 ),
-                                builder: (
-                                  RunMutation runMutation,
-                                  QueryResult? result,
-                                ) {
-                                  if (result!.hasException) {
-                                    print(result.exception.toString());
-                                  }
-                                  if (result.isLoading) {
-                                    return Center(
-                                      child: CircularProgressIndicator(
-                                        color: Color(0xFF6464DA),
-                                      ),
-                                    );
-                                  }
-                                  return ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        primary: Color(0xFF6464DA),
-                                        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                                        minimumSize: Size(40,24),
-                                        ),
-                                    onPressed: () {
-                                      runMutation({
-                                        'deleteAnnouncementAnnouncementId2':
-                                            widget.announcement.id
-                                      });
-                                      Navigator.pop(context);
-                                      widget.refetchAnnouncement!();
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (BuildContext context) =>
-                                                  Announcements()));
-                                    },
-                                    child: Text(
-                                      "Delete",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 12.0),
+                                Mutation(
+                                    options: MutationOptions(
+                                      document: gql(deleteAnnouncement),
                                     ),
-                                  );
-                                }),
-                          ],
-                        ),
-                      ),
-                  ],
+                                    builder: (
+                                        RunMutation runMutation,
+                                        QueryResult? result,
+                                        ) {
+                                      if (result!.hasException) {
+                                        print(result.exception.toString());
+                                      }
+                                      if (result.isLoading) {
+                                        return Center(
+                                          child: CircularProgressIndicator(
+                                            color: Color(0xFF6464DA),
+                                          ),
+                                        );
+                                      }
+                                      return ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          primary: Color(0xFF6464DA),
+                                          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                          minimumSize: Size(40,24),
+                                        ),
+                                        onPressed: () {
+                                          runMutation({
+                                            'deleteAnnouncementAnnouncementId2':
+                                            widget.announcement.id
+                                          });
+                                          Navigator.pop(context);
+                                          widget.refetchAnnouncement!();
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (BuildContext context) =>
+                                                      Announcements()));
+                                        },
+                                        child: Text(
+                                          "Delete",
+                                          style: TextStyle(
+                                              color: Colors.white, fontSize: 12.0),
+                                        ),
+                                      );
+                                    }),
+                              ],
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
                 ),
+                  builder: (_, collapsed, expanded) =>
+                      Expandable(
+                        collapsed: collapsed, expanded: expanded,)
               ),
             ),
           );
