a51efc1cb86972d9ef515f7ea523c85f3cf2b7c6
Aman <ch20b010@smail.iitm.ac.in>
2022-03-11 18:39:29 +0530

    Deleted hostelSec mutation named file

--- lib/graphQL/createHostelSec_mutation.dart
+++
@@ -1,7 +0,0 @@
-class createHostelSec {
-  String  createSec = """
-  mutation(\$hostelId: String!, \$createSecInput: CreateSecInput!){
-  createSec(HostelId: \$hostelId, CreateSecInput: \$createSecInput)
-}
-  """;
-}
