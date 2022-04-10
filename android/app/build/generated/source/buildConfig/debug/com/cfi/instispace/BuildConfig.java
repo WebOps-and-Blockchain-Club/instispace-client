98818c8c116991a72651c93aead65774075d8ae5
JanithMS <janithms9920@gmail.com>
2022-04-11 01:33:21 +0530

    merged branch with bugs

---
+++ android/app/build/generated/source/buildConfig/debug/com/cfi/instispace/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cfi.instispace;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cfi.instispace";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
