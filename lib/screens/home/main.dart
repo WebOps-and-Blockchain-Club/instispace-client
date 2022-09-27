21c040c6139bf7b38f5158259b79272c2994b76e
JanithMS <janithms9920@gmail.com>
2022-09-27 21:33:21 +0530

    Notification config

--- lib/screens/home/main.dart
+++ lib/screens/home/main.dart
@@ -1,12 +1,16 @@
+import 'dart:typed_data';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:http/http.dart' as http;
 
 import '../../models/user.dart';
 import '../../main.dart';
 import '../../services/auth.dart';
 import '../../widgets/headers/drawer.dart';
+import '../hostel/main.dart';
+import '/widgets/helpers/navigate.dart';
 import 'events/events.dart';
 import 'main/home.dart';
 import 'lost_and_found.dart/main.dart';
@@ -30,6 +34,7 @@ class _HomeWrapperState extends State<HomeWrapper> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   int _selectedIndex = 2;
+  String? scrollTo;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -46,26 +51,40 @@ class _HomeWrapperState extends State<HomeWrapper> {
     });
   }
 
+  Future<Uint8List> _getByteArrayFromUrl(String url) async {
+    final http.Response response = await http.get(Uri.parse(url));
+    return response.bodyBytes;
+  }
+
   @override
   void initState() {
     getToken();
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
       if (notification != null && android != null) {
+        BigPictureStyleInformation? bigPictureStyleInformation;
+        if (message.data["image"] != null) {
+          final ByteArrayAndroidBitmap bigPicture = ByteArrayAndroidBitmap(
+              await _getByteArrayFromUrl(message.data["image"]));
+          bigPictureStyleInformation = BigPictureStyleInformation(bigPicture,
+              contentTitle: notification.title,
+              htmlFormatContentTitle: true,
+              summaryText: notification.body,
+              htmlFormatSummaryText: true);
+        }
         flutterLocalNotificationsPlugin.show(
             notification.hashCode,
             notification.title,
             notification.body,
             NotificationDetails(
-              android: AndroidNotificationDetails(
-                channel.id,
-                channel.name,
-                channelDescription: channel.description,
-                color: const Color(0xFF2F247B),
-                playSound: true,
-                icon: '@mipmap/ic_launcher',
-              ),
+              android: AndroidNotificationDetails(channel.id, channel.name,
+                  channelDescription: channel.description,
+                  color: const Color(0xFF2F247B),
+                  playSound: true,
+                  icon: '@mipmap/ic_launcher',
+                  styleInformation: bigPictureStyleInformation),
               iOS: const IOSNotificationDetails(
                 sound: 'default.wav',
                 presentAlert: true,
@@ -75,24 +94,63 @@ class _HomeWrapperState extends State<HomeWrapper> {
             ));
       }
     });
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null) {
-        showDialog(
-            context: context,
-            builder: (_) {
-              return AlertDialog(
-                title: Text(notification.title!),
-                content: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text(notification.body!)],
-                  ),
-                ),
-              );
-            });
+
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        setState(() {
+          switch (message.data["route"]) {
+            case "EVENT":
+              _selectedIndex = 3;
+              scrollTo = message.data["id"];
+              break;
+            case "NETOP":
+              _selectedIndex = 4;
+              scrollTo = message.data["id"];
+              break;
+            case "QUERY":
+              _selectedIndex = 1;
+              scrollTo = message.data["id"];
+              break;
+            case "LnF":
+              _selectedIndex = 0;
+              scrollTo = message.data["id"];
+              break;
+            case "HOSTEL":
+              navigate(context, HostelWrapper(user: widget.user));
+              break;
+            default:
+          }
+        });
       }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      setState(() {
+        switch (message.data["route"]) {
+          case "EVENT":
+            _selectedIndex = 3;
+            scrollTo = message.data["id"];
+            break;
+          case "NETOP":
+            _selectedIndex = 4;
+            scrollTo = message.data["id"];
+            break;
+          case "QUERY":
+            _selectedIndex = 1;
+            scrollTo = message.data["id"];
+            break;
+          case "LnF":
+            _selectedIndex = 0;
+            scrollTo = message.data["id"];
+            break;
+          case "HOSTEL":
+            navigate(context, HostelWrapper(user: widget.user));
+            break;
+          default:
+        }
+      });
     });
 
     super.initState();
@@ -101,10 +159,12 @@ class _HomeWrapperState extends State<HomeWrapper> {
   Widget body(int index, AuthService auth, UserModel user, scaffoldKey) {
     switch (index) {
       case 0:
-        return LostAndFoundPage(user: user, scaffoldKey: scaffoldKey);
+        return LostAndFoundPage(
+            user: user, scaffoldKey: scaffoldKey, scrollTo: scrollTo);
 
       case 1:
-        return QueriesPage(user: user, scaffoldKey: scaffoldKey);
+        return QueriesPage(
+            user: user, scaffoldKey: scaffoldKey, scrollTo: scrollTo);
 
       case 2:
         return HomePage(
@@ -114,10 +174,12 @@ class _HomeWrapperState extends State<HomeWrapper> {
             scaffoldKey: scaffoldKey);
 
       case 3:
-        return EventsPage(user: user, scaffoldKey: scaffoldKey);
+        return EventsPage(
+            user: user, scaffoldKey: scaffoldKey, scrollTo: scrollTo);
 
       case 4:
-        return NetopsPage(user: user, scaffoldKey: scaffoldKey);
+        return NetopsPage(
+            user: user, scaffoldKey: scaffoldKey, scrollTo: scrollTo);
 
       default:
         return HomePage(
