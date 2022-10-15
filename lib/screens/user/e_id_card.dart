fb1e58e38a984ff13f5488dd4767935d0b83a387
Rasagnya <gudipudirasagnya@gmail.com>
2022-10-15 06:40:12 +0530

    E-ID Card UI change

--- lib/screens/user/e_id_card.dart
+++ lib/screens/user/e_id_card.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:client/widgets/headers/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '../../models/user.dart';
+import '../../widgets/button/icon_button.dart';
 
 class EIDCard extends StatefulWidget {
   final UserModel user;
@@ -16,7 +18,6 @@ class EIDCard extends StatefulWidget {
 class _EIDCardState extends State<EIDCard> {
   @override
   void initState() {
-    _portraitModeOnly();
     super.initState();
   }
 
@@ -25,113 +26,112 @@ class _EIDCardState extends State<EIDCard> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset(
-                    'assets/iitm_logo.png',
-                    height: 74,
-                    width: 74,
-                  ),
-                  const SizedBox(width: 16),
-                  Column(
-                    children: [
-                      Text(
-                        'भारतीय प्रौद्योगिकी संस्थान मद्रास, चेन्नई-600036',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        '(एमएचआरडी, जीओआई के तहत एक स्वायत्त संस्थान)',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        'Indian Institute of Technology Madras, Chennai-600036',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        '(An Autonomous Institution under MHRD, GOI)',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                    ],
-                  ),
-                ],
+              CustomAppBar(
+                  title: "E-ID Card",
+                  leading: CustomIconButton(
+                    icon: Icons.arrow_back,
+                    onPressed: () => Navigator.of(context).pop(),
+                  )),
+              const SizedBox(height: 30),
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: <Color>[
+                          Color.fromRGBO(165, 202, 114, 1),
+                          Color.fromRGBO(154, 169, 101, 1),
+                          Color.fromRGBO(149, 171, 96, 1),
+                        ]),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/iitm_logo.png',
+                      height: 74,
+                      width: 74,
+                    ),
+                    const SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'भारतीय प्रौद्योगिकी संस्थान मद्रास, चेन्नई-600036',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          '(एमएचआरडी, जीओआई के तहत एक स्वायत्त संस्थान)',
+                          style: Theme.of(context).textTheme.bodySmall,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Indian Institute of Technology Madras, Chennai-600036',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          '(An Autonomous Institution under MHRD, GOI)',
+                          style: Theme.of(context).textTheme.bodySmall,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ConstrainedBox(
+                        constraints: const BoxConstraints(
+                            maxWidth: 160,
+                            minWidth: 160,
+                            maxHeight: 160,
+                            minHeight: 160),
+                        child: CachedNetworkImage(
+                          imageUrl: widget.user.photo,
+                          placeholder: (_, __) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (_, __, ___) =>
+                              const Icon(Icons.account_circle_rounded),
+                        )),
+                    const SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.user.ldapName!,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          widget.user.roll!.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          widget.user.program!,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          widget.user.department!,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ConstrainedBox(
-                      constraints: const BoxConstraints(
-                          maxWidth: 160,
-                          minWidth: 160,
-                          maxHeight: 160,
-                          minHeight: 160),
-                      child: CachedNetworkImage(
-                        imageUrl: widget.user.photo,
-                        placeholder: (_, __) =>
-                            const CircularProgressIndicator(),
-                        errorWidget: (_, __, ___) =>
-                            const Icon(Icons.account_circle_rounded),
-                      )),
-                  const SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.user.ldapName!,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        widget.user.roll!.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        widget.user.program!,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        widget.user.department!,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                    ],
-                  )
-                ],
-              )
             ],
           ),
         ),
       ),
     );
   }
-
-  @override
-  void dispose() {
-    _enableRotation();
-    super.dispose();
-  }
-}
-
-void _portraitModeOnly() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
-}
-
-void _enableRotation() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
 }
