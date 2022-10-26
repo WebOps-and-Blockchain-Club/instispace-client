23f4d568450d5c0ea323ec10c7419e9cc3721fbe
JanithMS <janithms9920@gmail.com>
2022-10-26 17:48:33 +0530

    Created Notification Page

---
+++ lib/widgets/buttom_sheet/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Widget buildSheet(
+    BuildContext context, Widget Function(ScrollController controller) child) {
+  return makeDimissible(
+      context: context,
+      child: DraggableScrollableSheet(
+        initialChildSize: 0.7,
+        minChildSize: 0.5,
+        maxChildSize: 0.8,
+        builder: (_, controller) => Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: child(controller),
+        ),
+      ));
+}
+
+Widget makeDimissible({required Widget child, required BuildContext context}) =>
+    GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => Navigator.of(context).pop(),
+      child: GestureDetector(
+        onTap: () {},
+        child: child,
+      ),
+    );
