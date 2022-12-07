f45afee76dee19cf8d9ae44c89bb99cb825142cf
Rasagnya <gudipudirasagnya@gmail.com>
2022-12-07 22:59:51 +0530

    time table and academic calendar maps

---
+++ lib/utils/time_table.dart
@@ -0,0 +1,64 @@
+final time_table = {
+  'monday': [
+    'A',
+    'B',
+    'C',
+    'D',
+    'G',
+    ['P', 'H', 'H1'],
+    ['P', 'M', 'M2'],
+    ['J', 'J3']
+  ],
+  'tuesday': [
+    'B',
+    'C',
+    'D',
+    'E',
+    'A',
+    ['Q', 'M', 'M1'],
+    ['Q', 'H', 'H2'],
+    'F'
+  ],
+  'wednesday': [
+    'C',
+    'D',
+    'E',
+    'F',
+    'B',
+    ['R', 'J', 'J1'],
+    ['R', 'K', 'K2'],
+    'G'
+  ],
+  'thursday': [
+    'E',
+    'F',
+    'G',
+    'A',
+    'D',
+    ['S', 'L', 'L1'],
+    ['S', 'J', 'J2'],
+    ['H', 'H3']
+  ],
+  'friday': [
+    'F',
+    'G',
+    'A',
+    'B',
+    'C',
+    ['T', 'K', 'K1'],
+    ['T', 'L', 'L2'],
+    'E'
+  ]
+};
+
+final holidays = {
+  '2022-08-09': 'Muharram',
+  '2022-08-15': 'Independence Day',
+  '2022-08-31': 'Ganesh Chaturthi',
+  '2022-09-08': 'Onam',
+  '2022-10-02': 'Gandhi Jayanthi',
+  '2022-10-05': 'Dussehra',
+  '2022-10-24': 'Deepavali',
+  '2022-11-08': "Guru Nanak's Birthday",
+  '2022-12-25': 'Christmas'
+};
